79e96f58a181aaa122e368c0d9615bf164b33ea1
Larhard <larhard@gmail.com>
2015-06-19 23:10:58 +0200

    launcher: launch adapter and tui

--- src/main/java/com/elgassia/bridge/Bridge.java
+++ src/main/java/com/elgassia/bridge/Bridge.java
@@ -1,7 +1,21 @@
 package com.elgassia.bridge;
 
+import com.elgassia.bridge.adapter.Adapter;
+import com.elgassia.bridge.view.View;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Bridge {
     public static void main(String[] args) {
-        System.out.println("It Works");
+        Adapter main_adapter = new com.elgassia.bridge.adapter.main.Adapter();
+
+        List<View> views = Arrays.asList(new View[] {
+                new com.elgassia.bridge.view.tui.View(),
+        });
+
+        for (View view : views) {
+            view.init(main_adapter);
+        }
     }
 }
