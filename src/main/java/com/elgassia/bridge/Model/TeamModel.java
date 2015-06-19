8a1ea3bbb6cee1e61a7676ad8bc83e2aa2f9b96b
vereena42 <vereena42@gmail.com>
2015-06-19 23:04:20 +0200

    TeamModel setTeam(user,team) method added.

--- src/main/java/com/elgassia/bridge/Model/TeamModel.java
+++ src/main/java/com/elgassia/bridge/Model/TeamModel.java
@@ -1,12 +1,22 @@
 package com.elgassia.bridge.Model;
 
+import java.util.Observable;
+
 /**
  * Created by vereena on 6/19/15.
  */
-public class TeamModel {
+public class TeamModel extends Observable{
     char state;
+    String players[]=new String[4];
+    int playerTeam[]=new int[4];
     TeamModel()
     {
+        players[0]="user1";
+        players[1]="user2";
+        players[2]="user3";
+        players[3]="user4";
+        for(int i=0;i<4;i++)
+            playerTeam[i]=-1;
         state='0';
     }
     LobbyModel getLobbyModel()
@@ -40,5 +50,26 @@ public class TeamModel {
             return new GameOverModel(this);
         }
         return null;
+    }
+    private int countTeamPlayers(int team)
+    {
+        int counter=0;
+        for (int i=0;i<4;i++)
+        {
+            if (playerTeam[i]==team)
+                counter++;
+        }
+        return counter;
+    }
+    boolean setTeam(int user,int team)
+    {
+        if(countTeamPlayers(team)<2)
+        {
+            playerTeam[user]=team;
+            setChanged();
+            notifyObservers();
+            return true;
+        }
+        return false;
     }
 }
