b31d6172b204b1fc8046f560ba056a01e5c14e90
vereena42 <vereena42@gmail.com>
2015-06-19 22:34:50 +0200

    CreditsModel added. :*

---
+++ src/main/java/com/elgassia/bridge/Model/CreditsModel.java
@@ -0,0 +1,17 @@
+package com.elgassia.bridge.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by vereena on 6/19/15.
+ */
+public class CreditsModel {
+    List<String> getCredits()
+    {
+        ArrayList<String>creditsList=new ArrayList<>();
+        creditsList.add("Bartłomiej Puget");
+        creditsList.add("Dominika Salawa");
+        return creditsList;
+    }
+}
