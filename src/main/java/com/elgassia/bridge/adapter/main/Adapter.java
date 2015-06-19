de9f2ad5ebcde747b9d8861daaf1bb44fefb8a78
Larhard <larhard@gmail.com>
2015-06-19 23:10:48 +0200

    add main adapter

---
+++ src/main/java/com/elgassia/bridge/adapter/main/Adapter.java
@@ -0,0 +1,10 @@
+package com.elgassia.bridge.adapter.main;
+
+import java.util.Observable;
+
+public class Adapter extends com.elgassia.bridge.adapter.Adapter {
+    @Override
+    public void update(Observable observable, Object o) {
+
+    }
+}
