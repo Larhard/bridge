080cda8a323f11682cab3a66cf15b7f9ff551cdd
Larhard <larhard@gmail.com>
2015-06-19 10:59:39 +0200

    create project structure

---
+++ src/main/java/com/elgassia/bridge/Bridge.java
@@ -0,0 +1,7 @@
+package com.elgassia.bridge;
+
+public class Bridge {
+    public static void main(String[] args) {
+        System.out.println("It Works");
+    }
+}
---
+++ src/test/java/com/elgassia/bridge/ExampleTest.java
@@ -0,0 +1,8 @@
+package com.elgassia.bridge;
+
+import junit.framework.TestCase;
+
+public class ExampleTest extends TestCase {
+    public void testNothing() {
+    }
+}
