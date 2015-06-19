a5f121f13f3dd8528d763db13dd9afc0f5cdf105
vereena42 <vereena42@gmail.com>
2015-06-19 23:36:40 +0200

    LobbyModel ready(user) method added.

--- src/main/java/com/elgassia/bridge/Model/LobbyModel.java
+++ src/main/java/com/elgassia/bridge/Model/LobbyModel.java
@@ -7,25 +7,50 @@ import java.util.Random;
  */
 public class LobbyModel {
     TeamModel teamModel;
+    boolean readyUsers[]=new boolean[4];
     LobbyModel(TeamModel teamModel)
     {
+        for(int i=0;i<4;i++)
+            readyUsers[i]=false;
         this.teamModel=teamModel;
     }
     boolean setName(int user,String name)
     {
-        return teamModel.setName(user,name);
+        if(readyUsers[user]==false) {
+            teamModel.setName(user, name);
+            return true;
+        }
+        return false;
     }
     boolean setTeam(int user,int team)
     {
-        return teamModel.setTeam(user,team);
+        if(readyUsers[user]==false) {
+            return teamModel.setTeam(user, team);
+        }
+        return false;
     }
     boolean randomTeam(int user)
     {
-        int team=new Random().nextInt(2);
-        while (!setTeam(user,team))
-        {
-            team=new Random().nextInt(2);
+        if(readyUsers[user]==false) {
+            int team = new Random().nextInt(2);
+            while (!setTeam(user, team)) {
+                team = new Random().nextInt(2);
+            }
+            return true;
         }
+        return false;
+    }
+    boolean ready(int user)
+    {
+        if (teamModel.getUserTeam(user)==-1)
+            return false;
+        readyUsers[user]=true;
+        for(int i=0;i<4;i++)
+        {
+            if(readyUsers[i]==false)
+                return true;
+        }
+        teamModel.changeGameState('G');
         return true;
     }
 }
