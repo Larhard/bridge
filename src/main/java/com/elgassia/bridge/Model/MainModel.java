1a3a519f5376261ce455183e258ea781a3f0d46b
vereena42 <vereena42@gmail.com>
2015-06-19 22:33:22 +0200

    MainModel structure added.!

---
+++ src/main/java/com/elgassia/bridge/Model/MainModel.java
@@ -0,0 +1,15 @@
+package com.elgassia.bridge.Model;
+
+/**
+ * Created by vereena on 6/19/15.
+ */
+public class MainModel {
+    TeamModel newGame()
+    {
+        return new TeamModel();
+    }
+    CreditsModel getCredits()
+    {
+        return new CreditsModel();
+    }
+}
