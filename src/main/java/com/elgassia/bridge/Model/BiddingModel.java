744aa629be68bd5eff24c9c1f9ef07685fd48ee5
vereena42 <vereena42@gmail.com>
2015-06-20 19:02:22 +0200

    BiddingModel - bidding with CARD added.

--- src/main/java/com/elgassia/bridge/Model/BiddingModel.java
+++ src/main/java/com/elgassia/bridge/Model/BiddingModel.java
@@ -25,13 +25,45 @@ public class BiddingModel {
     {
         return teamModel.getPlayerCards();
     }
+    private Bid getLastCard()
+    {
+        Bid card=null;
+        for(int i=biddingHistory.size()-1;i>=0;i--)
+        {
+            if(biddingHistory.get(i).getType()==BidType.CARD)
+            {
+                card=biddingHistory.get(i);
+                break;
+            }
+        }
+        return card;
+    }
+    private void addBid(Bid bid)
+    {
+        biddingHistory.add(bid);
+        currentPlayer++;
+        if(currentPlayer==4)
+            currentPlayer=0;
+    }
     boolean bid(Bid bid,int user)
     {
         if(user!=currentPlayer)
             return false;
         if(bid.getType()==BidType.CARD)
         {
-
+            Bid lastCard=getLastCard();
+            if(lastCard==null)
+            {
+                addBid(bid);
+                return true;
+            }
+            if(bid.compareTo(lastCard)>0)
+            {
+                addBid(bid);
+                return true;
+            }
+            else
+                return false;
         }
         else if(bid.getType()==BidType.CONTRA)
         {
