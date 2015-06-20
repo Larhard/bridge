0be7a88a774ae4b12a1f7ac488d776c4c1a1c321
vereena42 <vereena42@gmail.com>
2015-06-20 18:20:24 +0200

    Card class added.

---
+++ src/main/java/com/elgassia/bridge/Model/Card.java
@@ -0,0 +1,36 @@
+package com.elgassia.bridge.Model;
+
+/**
+ * Created by vereena on 6/20/15.
+ */
+public class Card implements Comparable<Card> {
+
+    public static enum Rank {
+        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE;
+    }
+    public static enum Suit {
+        SPADES, HEARTS, DIAMONDS, CLUBS;
+    }
+    private Rank rank;
+    private Suit suit;
+
+    Card(Rank rank,Suit suit)
+    {
+        this.rank=rank;
+        this.suit=suit;
+    }
+    Rank getRank()
+    {
+        return this.rank;
+    }
+    Suit getSuit()
+    {
+        return this.suit;
+    }
+    @Override
+    public int compareTo(Card o) {
+        if(this.suit.compareTo(o.getSuit())!=0)
+            return this.suit.compareTo(o.getSuit());
+        return this.rank.compareTo(o.getRank());
+    }
+}
