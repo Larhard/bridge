b19391fc76e7d70533d87492bf6a026c5ae2b792
Larhard <larhard@gmail.com>
2015-06-22 00:01:05 +0200

    TUI: start new game

--- src/main/java/com/elgassia/bridge/view/tui/View.java
+++ src/main/java/com/elgassia/bridge/view/tui/View.java
@@ -1,14 +1,18 @@
 package com.elgassia.bridge.view.tui;
 
 import com.elgassia.bridge.adapter.Adapter;
+import com.elgassia.bridge.view.tui.scenes.LobbyScene;
 import com.elgassia.bridge.view.tui.scenes.MainScene;
 
+import java.util.EnumMap;
 import java.util.Observable;
 import java.util.Scanner;
 
 public class View implements com.elgassia.bridge.view.View {
     private Scene scene;
     private Adapter adapter;
+
+    private EnumMap<Adapter.State, Class<? extends Scene>> state_map = new EnumMap<>(Adapter.State.class);
 
     private class ReaderThread extends Thread {
         private final View view;
@@ -44,14 +48,25 @@ public class View implements com.elgassia.bridge.view.View {
 
     @Override
     public void init(Adapter adapter) {
+        state_map.put(Adapter.State.MAIN_MENU, MainScene.class);
+        state_map.put(Adapter.State.LOBBY, LobbyScene.class);
+
         this.adapter = adapter;
         adapter.addObserver(this);
 
-        scene = new MainScene();
-        scene.init(this);
+        setScene(state_map.get(Adapter.State.MAIN_MENU));
 
         readerThread = new ReaderThread(this);
         readerThread.start();
+    }
+
+    private void setScene(Class<? extends Scene> scene_class) {
+        try {
+            scene = scene_class.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        scene.init(this);
     }
 
     public void exit() {
@@ -68,6 +83,12 @@ public class View implements com.elgassia.bridge.view.View {
 
     @Override
     public void update(Observable observable, Object o) {
-
+        if (observable == adapter) {
+            Class<? extends Scene> new_scene = state_map.get(adapter.getState());
+            assert new_scene != null;
+            if (! scene.getClass().equals(new_scene)) {
+                setScene(new_scene);
+            }
+        }
     }
 }
--- src/main/java/com/elgassia/bridge/view/tui/scenes/LobbyScene.java
+++ src/main/java/com/elgassia/bridge/view/tui/scenes/LobbyScene.java
@@ -3,14 +3,10 @@ package com.elgassia.bridge.view.tui.scenes;
 import com.elgassia.bridge.view.tui.Commands;
 import com.elgassia.bridge.view.tui.Scene;
 import com.elgassia.bridge.view.tui.View;
-import com.elgassia.bridge.view.tui.commands.Credits;
-import com.elgassia.bridge.view.tui.commands.NewGame;
 
-public class MainScene extends Scene {
+public class LobbyScene extends Scene {
     @Override
     protected void prepareCommands(Commands commands, View view) {
         super.prepareCommands(commands, view);
-        commands.add("credits", new Credits(view.getAdapter()));
-        commands.add("new_game", new NewGame(view.getAdapter()));
     }
 }
