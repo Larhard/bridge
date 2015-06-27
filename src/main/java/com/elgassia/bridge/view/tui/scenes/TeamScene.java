825025b4e5a7f88f6cfd728c926aa0ed8f326cf2
Larhard <larhard@gmail.com>
2015-06-27 18:14:05 +0200

    [rename] LobbyScene -> TeamScene

---
+++ src/main/java/com/elgassia/bridge/view/tui/scenes/TeamScene.java
@@ -0,0 +1,25 @@
+package com.elgassia.bridge.view.tui.scenes;
+
+import com.elgassia.bridge.view.tui.Commands;
+import com.elgassia.bridge.view.tui.Scene;
+import com.elgassia.bridge.view.tui.View;
+import com.elgassia.bridge.view.tui.commands.*;
+
+public class TeamScene extends Scene {
+    @Override
+    protected void prepareCommands(Commands commands, View view) {
+        super.prepareCommands(commands, view);
+        switch (view.getAdapter().getTeamAdapter().getState()) {
+            case LOBBY:
+                commands.add("next", new NextPlayer(view.getAdapter().getTeamAdapter()));
+                commands.add("set_name", new SetName(view.getAdapter().getTeamAdapter().getLobbyAdapter()));
+                commands.add("start", new StartGame(view.getAdapter().getTeamAdapter().getLobbyAdapter()));
+                commands.add("random_teams", new SetRandomTeams(view.getAdapter().getTeamAdapter().getLobbyAdapter()));
+                commands.add("set_team", new SetTeam(view.getAdapter().getTeamAdapter().getLobbyAdapter()));
+                System.out.println("Player: " + view.getAdapter().getTeamAdapter().getName());
+                break;
+            default:
+                assert false;
+        }
+    }
+}
